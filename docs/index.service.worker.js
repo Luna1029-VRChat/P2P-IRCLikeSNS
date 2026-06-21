65706339d509a5628e4d747a291fb04ceb74543d
宵猫ルナ <stella1029.star@gmail.com>
2026-06-21 18:38:27 +0900

    Update WebClient

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1782032761|3854676';
+const CACHE_VERSION = '1782034671|100129392277';
 /** @type {string} */
 const CACHE_PREFIX = 'P2P-IRCLikeSNS-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
